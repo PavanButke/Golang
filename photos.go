d5fc3fd36e0c93fdf8f9934b552337977c9623d7
PavanButke <75151054+PavanButke@users.noreply.github.com>
2021-10-30 14:39:51 +0000

    Gallery02

--- photos.go
+++ photos.go
@@ -6,23 +6,27 @@ import (
 	"log"
 	"strings"
 
-	"fyne.io/fyne"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	wdw := a.NewWindow("Photos")
 	wdw.Resize(fyne.NewSize(800, 600))
-	root_src := "/config/Downloads/"
+
+	root_src := "/config/Desktop/Golang/Gallery/"
 
 	files, err := ioutil.ReadDir(root_src)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	var picsArr []string
+	picsArr = make([]string, 5, 8)
 
 	for _, file := range files {
 		fmt.Println(file.Name(), file.IsDir())
@@ -35,8 +39,11 @@ func main() {
 		}
 	}
 
-	image := canvas.NewImageFromFile(picsArr[0])
-	wdw.SetContent(image)
+	Tabs := container.NewAppTabs(
+		container.NewTabItem("Image1", canvas.NewImageFromFile(picsArr[3])),
+		container.NewTabItem("Tab 2", widget.NewLabel("World!")),
+	)
+	wdw.SetContent(Tabs)
 
 	wdw.ShowAndRun()
 }
